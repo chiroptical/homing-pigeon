88b023ccfcaf2a9aebd5ee53b65e1a6e042404bc
Barry Moore <chiroptical@gmail.com>
2022-02-23 08:07:36 -0500

    Add failing arrayfire example

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
 import HomingPigeon
+import qualified ArrayFire as A
+import Control.Exception (catch)
 
-main =
-  print $ "Hello from " ++ doHomingPigeon ++ "!"
+main :: IO ()
+main = print newArray `catch` (\(e :: A.AFException) -> print e)
+  where
+    newArray = A.matrix @Double (2,2) [ [1..], [1..] ] * A.matrix @Double (2,2) [ [2..], [2..] ]
