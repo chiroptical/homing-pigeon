f7def5f00a8566def8f378f36b6a1de7d0322f70
Barry Moore <chiroptical@gmail.com>
2022-02-27 14:43:52 -0500

    Working on packaging pytorch

--- flake.nix
+++ flake.nix
@@ -4,32 +4,26 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    hasktorch = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-      inputs.utils.follows = "flake-utils";
-      url = "github:hasktorch/hasktorch";
-    };
+    # hasktorch = {
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    #   inputs.flake-utils.follows = "flake-utils";
+    #   inputs.utils.follows = "flake-utils";
+    #   url = "github:hasktorch/hasktorch";
+    # };
   };
 
-  outputs = { self, nixpkgs, flake-utils, hasktorch, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         pkgs = import nixpkgs {
           inherit system;
           config.allowBroken = true;
         };
-        homingPigeon = import ./homing-pigeon.nix pkgs;
+        homingPigeon = pkgs.callPackage ./homing-pigeon.nix { };
 
       in
-      rec {
-
-        packages = flake-utils.lib.flattenTree {
-          inherit homingPigeon;
-        };
-
+      {
         devShell = import ./shell.nix pkgs;
-
-        defaultPackage = packages.homingPigeon;
+        defaultPackage = homingPigeon;
       });
 }
--- homing-pigeon.nix
+++ homing-pigeon.nix
@@ -1,1 +1,26 @@
-pkgs: pkgs.haskell.packages.ghc8107.callCabal2nix "homing-pigeon" ./. { }
+{ pkgs, fetchgit, ... }:
+let
+  hasktorch-git = fetchgit {
+    url = "https://github.com/hasktorch/hasktorch";
+    rev = "5f97cd3fa1647092de85e4675aebdc3da1967098";
+    sha256 = "sha256-w/CLe6Z1L3Lc5qrlk6QlSt0xzs6Jp/+BSuQBAwQBR/w=";
+  };
+
+  pytorch = pkgs.python39Packages.pytorch;
+
+  ghc = pkgs.haskell.packages.ghc8107.extend (self: super: rec {
+    libtorch-ffi-helper =
+      self.callCabal2nix "libtorch-ffi-helper" "${hasktorch-git}/libtorch-ffi-helper" { };
+
+    libtorch-ffi =
+      self.callCabal2nix "libtorch-ffi" "${hasktorch-git}/libtorch-ffi" {
+        c10 = pytorch;
+        torch = pytorch;
+        torch_cpu = pytorch;
+      };
+
+    hasktorch = self.callCabal2nix "hasktorch" "${hasktorch-git}" { };
+  });
+
+in
+ghc.callCabal2nix "homing-pigeon" ./. { }
